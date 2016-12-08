5b54cae3137cad54c021b8368c9b14b697caa2bc
Bin CHEN <setriones@gmail.com>
2016-12-08 13:42:08 +0800

    collect libyuv headers

--- pack.sh
+++ pack.sh
@@ -1,16 +1,17 @@
-#!/bin/bash -e
+#!/bin/bash
 
 dir=${PWD}/package
 objdir=${dir}/lib
 headerdir=${dir}/include
 rm -rf ${dir}
 mkdir -p ${dir}
-mkdir -p ${objdir}
+mkdir -p ${objdir} 
 mkdir -p ${headerdir}
 
-find src/webrtc src/chromium/src/third_party/jsoncpp -name *.h -exec cp --parents '{}' ${headerdir} ';'
+find src/webrtc src/chromium/src/third_party/jsoncpp src/chromium/src/third_party/libyuv/ -name *.h -exec cp --parents '{}' ${headerdir} ';'
 mv ${headerdir}/src/* ${headerdir}
 mv ${headerdir}/chromium/src/third_party/jsoncpp/source/include/* ${headerdir}
+mv ${headerdir}/chromium/src/third_party/libyuv/include/libyuv ${headerdir}
 rm -rf ${headerdir}/src ${headerdir}/chromium
 cd ./src/out/Default/obj/
 #find . -name "*.o" |grep -v unittest | grep -v "/webrtc/test" | grep -v "/testing/" | xargs -t -i ar -crs ${objdir}/libwebrtc_full.a {} 1>log 2>&1
@@ -18,7 +19,7 @@ cd ./src/out/Default/obj/
 #find . -name "*.a" | xargs -t -i cp --backup=existing --suffix=.old {} ${objdir}/"$(basename "$(dirname {})")".{}
 lst=(`find . -name "*.o" |grep -v unittest | grep -v "/webrtc/test" | grep -v "/testing/" | grep -v "_test"`)
 for file in ${lst[@]}; do
-	echo ${file}
+	#echo ${file}
 	p="$(basename "$(dirname ${file})")"
 	f=`basename ${file}`
 	newfile=${p}.${f}
@@ -26,3 +27,6 @@ for file in ${lst[@]}; do
 done
 cd ${objdir}
 ar -crs libwebrtc_full.a *.o
+rm -rf *.o
+cd ${dir}
+tar cf webrtc.tar ./*
